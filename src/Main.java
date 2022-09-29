146e80004b3a28bcc2d6b8e1e2a9b679b7238432
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 19:50:39 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        n = input.nextInt();
+
+        for(int i = 1; i<=n; i++) {
+
+            for(int j=1; j<=(n-i); j++) {
+                System.out.print(" ");
+            }
+
+            for(int k = 1;  k<=(2*i)- 1; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for(int a = 1; a < n; a++) {
+            for(int b = 1; b <= a; b++) {
+                System.out.print(" ");
+            }
+
+            for(int c = 1; c <= 2*(n-a) - 1; c++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+        }
     }
 }
